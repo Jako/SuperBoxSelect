bba62bb10b369466d7619f47e381982b4cc3a69c
Darryl Meganoski <darryl@smargasy.com>
2016-08-16 10:17:24 -0400

    added option for custom processor

--- assets/components/superboxselect/connector.php
+++ assets/components/superboxselect/connector.php
@@ -11,13 +11,20 @@ require_once dirname(dirname(dirname(dirname(__FILE__)))) . '/config.core.php';
 require_once MODX_CORE_PATH . 'config/' . MODX_CONFIG_KEY . '.inc.php';
 require_once MODX_CONNECTORS_PATH . 'index.php';
 
-$corePath = $modx->getOption('superboxselect.core_path', null, $modx->getOption('core_path') . 'components/superboxselect/');
-$superboxselect = $modx->getService('superboxselect', 'SuperBoxSelect', $corePath . 'model/superboxselect/', array(
-    'core_path' => $corePath
-));
+if (($package = $_POST['packageName']) && ($class = $_POST['className'])) {
+    $corePath = $modx->getOption($package . '.core_path', null, $modx->getOption('core_path') . 'components/' . $package . '/');
+    $package = $modx->getService($package, $class, $corePath . 'model/' . $package . '/', array(
+        'core_path' => $corePath
+    ));
+} else {
+    $corePath = $modx->getOption('superboxselect.core_path', null, $modx->getOption('core_path') . 'components/superboxselect/');
+    $package = $modx->getService('superboxselect', 'SuperBoxSelect', $corePath . 'model/superboxselect/', array(
+        'core_path' => $corePath
+    ));
+}
 
 // Handle request
 $modx->request->handleRequest(array(
-    'processors_path' => $superboxselect->getOption('processorsPath'),
+    'processors_path' => $corePath . 'processors/',
     'location' => ''
 ));
