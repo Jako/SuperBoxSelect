a62eb891f05aedf285aa796b6f169b94afa0fde2
Thomas Jakobi <thomas.jakobi@partout.info>
2019-04-03 08:33:17 +0200

    New translations setting.inc.php (Russian)

--- core/components/superboxselect/lexicon/ru/setting.inc.php
+++ core/components/superboxselect/lexicon/ru/setting.inc.php
@@ -7,6 +7,6 @@
  */
 
 $_lang['setting_superboxselect.advanced'] = 'Расширенный';
-$_lang['setting_superboxselect.advanced_desc'] = 'Display advanced TV settings (i.e. Package and Field Template).';
+$_lang['setting_superboxselect.advanced_desc'] = 'Показывать дополнительные настройки TV (например, шаблон пакета и поля).';
 $_lang['setting_superboxselect.debug'] = 'Отладка';
 $_lang['setting_superboxselect.debug_desc'] = 'Записывать отладочную информацию в лог ошибок MODX.';
