7511bcffc09010d14473983be669dac2a0070f0d
Thomas Jakobi <buero@treehillstudio.de>
2025-11-17 12:43:03 +0100

    Added translation using Weblate (Spanish)

---
+++ core/components/superboxselect/lexicon/es/default.inc.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Default Lexicon Entries for SuperBoxSelect
+ *
+ * @package superboxselect
+ * @subpackage lexicon
+ */
+$_lang['superboxselect'] = '';
+$_lang['superboxselect.allowedUsergroups'] = '';
+$_lang['superboxselect.allowedUsergroups_desc'] = '';
+$_lang['superboxselect.className'] = '';
+$_lang['superboxselect.className_desc'] = '';
+$_lang['superboxselect.customtable'] = '';
+$_lang['superboxselect.deniedUsergroups'] = '';
+$_lang['superboxselect.deniedUsergroups_desc'] = '';
+$_lang['superboxselect.fieldTpl'] = '';
+$_lang['superboxselect.fieldTpl_desc'] = '';
+$_lang['superboxselect.maxElements'] = '';
+$_lang['superboxselect.maxElements_desc'] = '';
+$_lang['superboxselect.maxElements_label'] = '';
+$_lang['superboxselect.maxElements_msg'] = '';
+$_lang['superboxselect.packageName'] = '';
+$_lang['superboxselect.packageName_desc'] = '';
+$_lang['superboxselect.pageSize'] = '';
+$_lang['superboxselect.pageSize_desc'] = '';
+$_lang['superboxselect.resources'] = '';
+$_lang['superboxselect.resourceTitleTpl'] = '';
+$_lang['superboxselect.resourceTitleTpl_desc'] = '';
+$_lang['superboxselect.selectedFields'] = '';
+$_lang['superboxselect.selectedFields_desc'] = '';
+$_lang['superboxselect.selectType'] = '';
+$_lang['superboxselect.selectType_desc'] = '';
+$_lang['superboxselect.sortBy'] = '';
+$_lang['superboxselect.sortBy_desc'] = '';
+$_lang['superboxselect.sortDir'] = '';
+$_lang['superboxselect.sortDir_desc'] = '';
+$_lang['superboxselect.stackItems'] = '';
+$_lang['superboxselect.stackItems_desc'] = '';
+$_lang['superboxselect.users'] = '';
+$_lang['superboxselect.userTitleTpl'] = '';
+$_lang['superboxselect.userTitleTpl_desc'] = '';
