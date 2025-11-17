1878267145347e651ce83228d59b22a17bbec79e
Thomas Jakobi <buero@treehillstudio.de>
2025-11-17 12:43:09 +0100

    Added translation using Weblate (Spanish)

---
+++ core/components/superboxselect/lexicon/es/setting.inc.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Setting Lexicon Entries for SuperBoxSelect
+ *
+ * @package superboxselect
+ * @subpackage lexicon
+ */
+$_lang['setting_superboxselect.advanced'] = '';
+$_lang['setting_superboxselect.advanced_desc'] = '';
+$_lang['setting_superboxselect.debug'] = '';
+$_lang['setting_superboxselect.debug_desc'] = '';
